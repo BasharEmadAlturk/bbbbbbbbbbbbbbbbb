b618fdd3d07bd04114fabcaa1d0d5bb10c2aa5f9
BasharEmadAlturk <163429121+BasharEmadAlturk@users.noreply.github.com>
2024-05-07 21:36:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using Restaurant.Models;
+using Restaurant.Repositories;
+
+namespace Restaurant
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews().AddSessionStateTempDataProvider(); // Session Config
+
+            builder.Services.AddDistributedMemoryCache();
+
+            builder.Services.AddSession(options =>
+            {
+                options.Cookie.Name = ".ShoppingCart.Session";
+                options.Cookie.IsEssential = true;
+            });
+
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            builder.Services.AddDbContext<RestaurantContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("Cs"))
+                );
+            // add identity service to work on users & admins, and declare which store you used
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options=>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<RestaurantContext>();
+
+
+            builder.Services.AddScoped<IGenericRepository<Product>, ProductRepository>();
+            builder.Services.AddScoped<IGenericRepository<Customer>, CustomerRepository>();
+            builder.Services.AddScoped<IGenericRepository<Category>, CategoryRepository>();
+            builder.Services.AddScoped<IGenericRepository<Order>, OrderRepository>();
+            builder.Services.AddScoped<IGenericRepository<OrderItems>, OrderItemsRepository>();
+
+            // configure and inject automapper
+            builder.Services.AddAutoMapper(typeof(Program));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();  // have valid account 
+            app.UseAuthorization();  // all users allow permission
+
+            app.UseSession(); // Usage of Session
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
